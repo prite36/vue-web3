f1d71ff6487e784fd985b311279fe0845b677820
prite36 <spritelemon36@hotmail.com>
2019-07-23 22:44:53 +0700

    no message

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const serveStatic = require("serve-static")
 const path = require('path');
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.get(/.*/, function(req, res){
+    res.dendfile(__dirname + "dist/index.html");
+})
 const port = process.env.PORT || 80;
 app.listen(port);
+console.log("server started..");
